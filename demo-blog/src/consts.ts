69979629ef37d3c4d0a6d5d8abd770f3d5d5a707
kidlatsia <kidlatsia@gmail.com>
2025-01-08 16:47:17 +0000

    added landing

--- demo-blog/src/consts.ts
+++ demo-blog/src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Passive Income for Dummy Blog';
+export const SITE_DESCRIPTION = 'Learn how to build passive income streams with simple, practical, and beginner-friendly advice. Discover strategies, tips, and insights to grow your financial freedom step by step.';
