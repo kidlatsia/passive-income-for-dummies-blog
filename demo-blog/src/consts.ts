abc97a70a1448fa44f167165be367152f7a6d269
kidlatsia <kidlatsia@gmail.com>
2025-01-10 01:06:24 +0000

    update header footer

--- demo-blog/src/consts.ts
+++ demo-blog/src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Passive Income for Dummy Blog';
+export const SITE_TITLE = 'Your Path to Freedom, Guided by Faith';
 export const SITE_DESCRIPTION = 'Learn how to build passive income streams with simple, practical, and beginner-friendly advice. Discover strategies, tips, and insights to grow your financial freedom step by step.';
